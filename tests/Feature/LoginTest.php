260db82f98044b67130bd0a79c86968b78e95053
Ruman Saleem <ruman63@gmail.com>
2019-09-05 23:23:44 +0530

    Logout test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginTest extends TestCase
 {
-
     use RefreshDatabase;
 
     /** @test */
@@ -19,10 +18,9 @@ class LoginTest extends TestCase
     }
 
     /** @test */
-    public function user_can_login_with_correct_credentials() {
-
+    public function user_can_login_with_correct_credentials()
+    {
         $user = factory(\App\User::class)->create([
-            // 'email' => 'test.ducs@gmail.com'
             'password' => bcrypt('secret')
         ]);
 
@@ -34,16 +32,25 @@ class LoginTest extends TestCase
         ])->assertRedirect('/');
 
         $this->assertTrue(Auth::check(), 'User was expected to login but was not.');
-
     }
 
     /** @test */
-    public function if_user_is_logged_in_redirect_to_home() {
-
+    public function if_user_is_logged_in_redirect_to_home()
+    {
         $user = factory(\App\User::class)->create();
         $this->be($user);
 
         $this->get('/login')->assertRedirect('/');
+    }
 
+    /** @test */
+    public function logged_in_user_can_logout()
+    {
+        $user = factory(\App\User::class)->create();
+        $this->be($user)->withoutExceptionHandling();
+
+        $this->post('/logout')->assertRedirect('/');
+
+        $this->assertFalse(auth()->check(), 'User was expected to be logged out, but was not logged out!');
     }
 }
