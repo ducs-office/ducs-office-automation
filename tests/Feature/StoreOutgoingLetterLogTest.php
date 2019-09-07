0a63b38737825d471be8833857fb6bdeda08a44c
Ruman Saleem <ruman63@gmail.com>
2019-09-08 02:44:51 +0530

    Validate date field cannot be null - Fail test

--- tests/Feature/StoreOutgoingLetterLogTest.php
+++ tests/Feature/StoreOutgoingLetterLogTest.php
@@ -33,4 +33,24 @@ class StoreOutgoingLetterLogTest extends TestCase
             
         $this->assertEquals(1, OutgoingLetterLog::count());
     }
+
+    /** @test */
+    public function request_validates_date_field_is_not_null()
+    {
+        try {
+            $this->be(factory(\App\User::class)->create());
+            $letter = factory(OutgoingLetterLog::class)->make(['date' => '']);
+        
+            $this->withoutExceptionHandling()
+                ->post('/outgoing-letter-logs', $letter->toArray());
+            
+            $this->fail('Empty date field was not validated.');
+        } catch(ValidationException $e) {
+            $this->assertArrayHasKey('date', $e->errors());
+            $this->assertEquals(0, OutgoingLetterLog::count());
+        } catch(\Exception $e) {
+            $this->fail('Empty date field was not validated.');
+        }
+    }
+
 }
