17f34f2f19e097b87529dab98910eb3255bcbaf0
Ruman Saleem <ruman63@gmail.com>
2019-09-08 01:19:25 +0530

    Fix Letter Log Factory sender relation

--- database/factories/OutgoingLetterLogFactory.php
+++ database/factories/OutgoingLetterLogFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\OutgoingLetterLog;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(OutgoingLetterLog::class, function (Faker $faker) {
@@ -10,8 +11,10 @@ $factory->define(OutgoingLetterLog::class, function (Faker $faker) {
         'date' => $faker->dateTime(),
         'type' => $faker->realText(50),
         'recipient' => $faker->name,
-        'sender_id' => rand(1, App\User::count()),
+        'sender_id' => function() {
+            return factory(User::class)->create()->id;
+        },
         'description' => $faker->realText(400),
-        'amount' => $faker->randomFloat(2,1),
+        'amount' => $faker->randomFloat(2, 10.00, 20000.00),
     ];
 });
