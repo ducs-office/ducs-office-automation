eef33e29df110a9fd28059698ec0811c6f2f622f
Ruman Saleem <ruman63@gmail.com>
2019-09-08 12:27:31 +0530

    Fix Factories memory overflow

--- database/factories/OutgoingLetterLogFactory.php
+++ database/factories/OutgoingLetterLogFactory.php
@@ -9,12 +9,12 @@ use Faker\Generator as Faker;
 $factory->define(OutgoingLetterLog::class, function (Faker $faker) {
     return [
         'date' => $faker->dateTime(),
-        'type' => $faker->realText(50),
+        'type' => $faker->words(3, true),
         'recipient' => $faker->name,
         'sender_id' => function() {
             return factory(User::class)->create()->id;
         },
-        'description' => $faker->realText(400),
+        'description' => $faker->paragraph,
         'amount' => $faker->randomFloat(2, 10.00, 20000.00),
     ];
 });
