be2883315d0555c82e3c3602de6e2529b3b67477
Swati-007 <42612721+Swati-007@users.noreply.github.com>
2019-09-09 14:59:08 +0530

    store letter logs validations - tests passing (#2)

--- app/Http/Controllers/OutgoingLetterLogsController.php
+++ app/Http/Controllers/OutgoingLetterLogsController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\OutgoingLetterLog;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class OutgoingLetterLogsController extends Controller
 {
@@ -14,14 +16,24 @@ class OutgoingLetterLogsController extends Controller
     
     protected function store(Request $request) 
     {
-        OutgoingLetterLog::create($request->only([
-            'date', 
-            'type', 
-            'recipient', 
-            'sender_id', 
-            'description', 
-            'amount'
-        ]));
+        $today = date('Y-m-d H:i:s');
+        $validData = $request->validate([
+            'date' => 'required|date_format:Y-m-d H:i:s|before_or_equal:'.$today,
+            'type' => 'required',
+            'recipient' =>  'required|',
+            'description' => 'nullable|string',
+            'amount' => 'nullable|numeric',
+            'sender_id' => [
+                'required',
+                function ($attribute, $value, $fail) {
+                    if(2===2) {
+                        $fail($attribute.' is invalid.');
+                    }
+                },
+            ],
+        ]);
+        
+        OutgoingLetterLog::create($validData);
         
         return redirect('/outgoing-letter-logs');
     }
