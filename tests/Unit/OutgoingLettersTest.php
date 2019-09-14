611d5b65c1e7e3779c310ac06f332a5caed8f52a
Swati-007 <swati.mcs18.du@gmail.com>
2019-09-14 19:11:17 +0530

    refactors unit tests

---
+++ tests/Unit/OutgoingLettersTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\OutgoingLetter;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class OutgoingLettersTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function date_attribute_is_casted_to_carbon_instance()
+    {
+        $outgoing_letter = factory(OutgoingLetter::class)->make(['date' => '2016-08-08']);
+
+        $this->assertInstanceOf(Carbon::class, $outgoing_letter->date);
+        $this->assertEquals('2016-08-08', $outgoing_letter->date->format('Y-m-d'));
+    }
+
+    /** @test */
+    public function letter_has_an_associated_sender_who_is_a_user()
+    {
+        $jack = factory(User::class)->create();
+        $outgoing_letter = factory(OutgoingLetter::class)->create(['sender_id' => $jack->id]);
+
+        $this->assertInstanceOf(BelongsTo::class, $outgoing_letter->sender(), 'sender method was expected to return a belongsTo relation');
+        $this->assertInstanceOf(User::class, $outgoing_letter->sender);
+        $this->assertTrue($outgoing_letter->sender->is($jack));
+    }
+}
