a92e3910de200415987bb961bb65b5195a1a93e1
Ruman Saleem <ruman63@gmail.com>
2019-09-17 16:57:02 +0530

    letters index UI refresh

--- resources/views/outgoing_letters/index.blade.php
+++ resources/views/outgoing_letters/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.master')
 @section('body')
-    <div class="m-4 page-card pb-2 overflow-x-auto">
-        <div class="flex items-baseline px-6 mb-4">
+    <div class="m-6 page-card pb-2 overflow-x-auto">
+        <div class="flex items-baseline px-6 pb-4 border-b mb-2">
             <h1 class="page-header mb-0 px-0 mr-4">Outgoing Letters</h1>
             <a href="/outgoing-letters/create" class="btn btn-magenta is-sm shadow-inset">
                 Create
             </a>
         </div>
         
-        <div class="flex justify-between items-end py-2 px-6">
+        <div class="flex justify-between items-end py-2 px-6 mb-2">
             <form method="GET" class="flex items-end">
                 <input type="text" name="after" 
                     placeholder="After date"
@@ -25,42 +25,15 @@
                 <button type="submit" class="btn btn-black is-sm">Apply Filter</button>
             </form>
         </div>
-        <table class="max-w-full border border-t-0 border-collapse text-left">
-            <thead>
-                <th class="bg-gray-200 pl-6 py-2">S.NO</th>
-                <th class="bg-gray-200 px-3 py-2">Date</th>
-                <th class="bg-gray-200 px-3 py-2">Sender</th>
-                <th class="bg-gray-200 px-3 py-2">Recipient</th>
-                <th class="bg-gray-200 px-3 py-2">Type</th>
-                <th class="bg-gray-200 pl-3 pr-6 py-2 text-right">Options</th>
-            </thead>
-            <tbody>
-                @foreach($outgoing_letters as $letter)
-                <tr class="hover:bg-gray-100">
-                    <td class="pl-6 pr-3 py-1 border-b table-fit">DU/CS/{{ str_pad($letter->id, 4, '0', STR_PAD_LEFT) }}</td>
-                    <td class="px-3 py-1 border-b table-fit">{{ $letter->date->format('d M, Y') }}</td>
-                    <td class="px-3 py-1 border-b">{{ $letter->sender->name }}</td>
-                    <td class="px-3 py-1 border-b">{{ $letter->recipient }}</td>
-                    <td class="px-3 py-1 border-b table-fit">{{ $letter->type }}</td>
-                    <td class="pl-3 pr-6 py-1 border-b table-fit text-right">
-                        <a href="/outgoing-letters/{{$letter->id}}/edit" 
-                            class="p-1 btn btn-blue"
-                            title="Edit">
-                            <feather-icon name="edit-3" class="h-current">Edit</feather-icon>
-                        </a>
-                        <form method = "POST" action="/outgoing-letters/{{$letter->id}}" >
-                            @csrf
-                            @method('DELETE')
-                            <button 
-                                type = "submit"
-                                class="p-1 border-2 bg-red-700 text-white border rounded">
-                                <feather-icon name="trash-2" class="h-current">Delete</feather-icon>    
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        @forelse($outgoing_letters as $letter)
+            @include('outgoing_letters.partials.letter', compact('letter'))
+        @empty
+            <div class="py-8 flex flex-col items-center justify-center text-gray-500">
+                <feather-icon name="frown" class="h-16"></feather-icon>
+                <p class="mt-4 mb-2  font-bold">
+                    Sorry! No Letters {{ count(request()->query()) ? 'found for your query.' : 'added yet.' }}
+                </p>
+            </div>
+        @endforelse
     </div>
 @endsection
---
+++ resources/views/outgoing_letters/partials/letter.blade.php
@@ -0,0 +1,39 @@
+<div class="px-6 py-4 hover:bg-gray-100 border-t">
+    <div class="flex items-center mb-1">
+        <h5 class="mr-12 text-gray-700 font-bold">{{ $letter->date->format('M d, Y') }}</h5>
+        <div class="flex items-center text-gray-700">
+            {{ $letter->sender->name }}
+            <feather-icon name="arrow-up-right"
+            stroke-width="3"
+            class="h-current text-green-600 mx-2">Sent to</feather-icon>
+            {{ $letter->recipient }}
+        </div>
+        <div class="ml-auto flex">
+            <a href="/outgoing-letters/{{$letter->id}}/edit" 
+                class="p-1 text-gray-500 hover:bg-gray-200 hover:text-blue-600 rounded mr-3" title="Edit">
+                <feather-icon name="edit-3" stroke-width="2.5" class="h-current">Edit</feather-icon>
+            </a>
+            <form method="POST" action="/outgoing-letters/{{$letter->id}}">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="p-1 hover:bg-gray-200 text-red-700 rounded">
+                    <feather-icon name="trash-2" stroke-width="2.5" class="h-current">Delete</feather-icon>
+                </button>
+            </form>
+        </div>
+    </div>
+    <h4 class="text-xl font-bold mb-1">{{ $letter->subject ?? 'Subject of Letter' }}</h4>
+    @isset($letter->amount)
+    <div class="flex items-end">
+        <p class="w-2/3 text-black-50">{{ $letter->description }}</p>
+        <div class="flex-1 px-4 text-xl font-bold text-right">
+            &#x20B9;{{ substr(number_format($letter->amount, 2), 0, -2) }}
+            <span class="text-sm">
+                {{ substr(number_format($letter->amount, 2), -2, 2) }}
+            </span>   
+        </div>
+    </div>
+    @else
+        <p class="text-black-50">{{ $letter->description }}</p>
+    @endisset
+</div>
