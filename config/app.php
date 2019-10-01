ecec0e2a64359cae9ae51b290d3b8f2acce5803f
Ruman Saleem <ruman63@gmail.com>
2019-10-01 21:16:17 +0530

    use Flash messages package by laracasts

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Laracasts\Flash\FlashServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Flash' => Laracasts\Flash\FlashNotifier::class,
     ],
 
 ];
