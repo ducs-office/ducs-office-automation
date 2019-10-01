08c062c0f088157392df0df4f979310a3d3d6965
Ruman Saleem <ruman63@gmail.com>
2019-10-01 15:45:37 +0530

    refactor search"

--- app/Http/Controllers/OutgoingLettersController.php
+++ app/Http/Controllers/OutgoingLettersController.php
@@ -13,21 +13,24 @@ class OutgoingLettersController extends Controller
     public function index(Request $request)
     {
         $filters = $request->query('filters');
-        
-        if($request->has('search') && request('search')!= '') {
-            $outgoing_letters = OutgoingLetter::applyFilter($filters)->where('subject','like','%'.request('search').'%')->orWhere('description','like','%'.request('search').'%')->orderBy('date','DESC')->get();
-         }else{
-            $outgoing_letters = OutgoingLetter::applyFilter($filters)->orderBy('date','DESC')->get();
-         }
+
+        $query = OutgoingLetter::applyFilter($filters);
+
+        if ($request->has('search') && request('search')!= '') {
+            $query->where('subject','like','%'.request('search').'%')
+                ->orWhere('description','like','%'.request('search').'%');
+        }
+
+        $outgoing_letters = $query->orderBy('date','DESC')->get();
 
         $recipients = OutgoingLetter::selectRaw('DISTINCT(recipient)')->get()->pluck('recipient')->toArray();
         $types = OutgoingLetter::selectRaw('DISTINCT(type)')->get()->pluck('type')->toArray();
         $senders = User::select('id', 'name')->whereIn('id', OutgoingLetter::selectRaw('DISTINCT(sender_id)'))->get()->toArray();
         $creators = User::select('id', 'name')->whereIn('id', OutgoingLetter::selectRaw('DISTINCT(creator_id)'))->get()->toArray();
-        
+
         return view('outgoing_letters.index', compact(
             'outgoing_letters',
-            'types', 
+            'types',
             'recipients',
             'creators',
             'senders'
@@ -38,7 +41,7 @@ class OutgoingLettersController extends Controller
     {
         return view('outgoing_letters.create');
     }
-    
+
     protected function store(Request $request)
     {
         $validData = $request->validate([
@@ -50,9 +53,9 @@ class OutgoingLettersController extends Controller
             'description' => 'nullable|string|max:400',
             'amount' => 'nullable|numeric',
         ]);
-        
+
         OutgoingLetter::create($validData + ['creator_id'=> Auth::id()]);
-        
+
         return redirect('/outgoing-letters');
     }
 
@@ -74,14 +77,14 @@ class OutgoingLettersController extends Controller
         ]);
 
         $outgoing_letter->update($validData);
-        
+
         return redirect('/outgoing-letters');
     }
 
-    public function destroy(OutgoingLetter $outgoing_letter) 
+    public function destroy(OutgoingLetter $outgoing_letter)
     {
         $outgoing_letter->delete();
-        
+
         return redirect('/outgoing-letters');
     }
 }
