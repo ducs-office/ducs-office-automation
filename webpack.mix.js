4ec68aa026dd584eabbd7234ec41c83f8814a3b2
Ruman Saleem <ruman63@gmail.com>
2019-11-26 23:50:19 +0530

    purge css in production

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,10 @@
 const process = require('process');
 const mix = require('laravel-mix');
 require('laravel-mix-tailwind');
+require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .tailwind('tailwind.config.js')
+    .purgeCss()
     .browserSync(process.env.APP_URL);
