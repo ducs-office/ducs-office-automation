3cbdcbc538287d085f22f7914a236732b859a0e7
gautamswati <swati.mcs18.du@gmail.com>
2019-12-23 00:12:45 +0530

    Inc Letter:ID changes acc to date

--- app/Http/Controllers/IncomingLettersController.php
+++ app/Http/Controllers/IncomingLettersController.php
@@ -8,6 +8,8 @@ use App\IncomingLetter;
 use App\OutgoingLetter;
 use Auth;
 use App\User;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Carbon;
 
 class IncomingLettersController extends Controller
 {
@@ -48,47 +50,6 @@ class IncomingLettersController extends Controller
         ));
     }
 
-
-    public function edit(IncomingLetter $incoming_letter)
-    {
-        return view('incoming_letters.edit', compact('incoming_letter'));
-    }
-
-    public function update(IncomingLetter $incoming_letter, Request $request)
-    {
-        $validData = $request->validate([
-            'date' => 'sometimes|required|date|before_or_equal:today',
-            'received_id' => 'sometimes|required|string',
-            'sender' => 'sometimes|required|string',
-            'recipient_id' => 'sometimes|required|exists:users,id',
-            'handovers' => 'sometimes|nullable|array',
-            'handovers.*' => 'exists:users,id',
-            'priority' => 'nullable|in:1,2,3',
-            'subject' => 'sometimes|required|string|max:80',
-            'description' => 'nullable|string|max:400',
-            'attachments' => 'sometimes|required|array|max:2',
-            'attachments.*' => 'file|max:200|mimes:jpeg,jpg,png,pdf'
-        ]);
-
-        $incoming_letter->update($validData);
-        if ($request->has('handovers')) {
-            $incoming_letter->handovers()->sync($validData['handovers']);
-        }
-
-        if ($request->hasFile('attachments')) {
-            $incoming_letter->attachments()->createMany(
-                array_map(function ($attachedFile) {
-                    return [
-                        'original_name' => $attachedFile->getClientOriginalName(),
-                        'path' => $attachedFile->store('/letter_attachments/incoming')
-                    ];
-                }, $request->file('attachments'))
-            );
-        }
-
-        return redirect('/incoming-letters');
-    }
-
     public function create()
     {
         return view('incoming_letters.create');
@@ -124,6 +85,59 @@ class IncomingLettersController extends Controller
                 ];
             }, request()->file('attachments'))
         );
+
+        return redirect('/incoming-letters');
+    }
+
+    public function edit(IncomingLetter $incoming_letter)
+    {
+        return view('incoming_letters.edit', compact('incoming_letter'));
+    }
+
+    public function update(IncomingLetter $incoming_letter, Request $request)
+    {
+        $validData = $request->validate([
+            'date' => 'sometimes|required|date|before_or_equal:today',
+            'received_id' => 'sometimes|required|string',
+            'sender' => 'sometimes|required|string',
+            'recipient_id' => 'sometimes|required|exists:users,id',
+            'handovers' => 'sometimes|nullable|array',
+            'handovers.*' => 'exists:users,id',
+            'priority' => 'nullable|in:1,2,3',
+            'subject' => 'sometimes|required|string|max:80',
+            'description' => 'nullable|string|max:400',
+            'attachments' => 'sometimes|required|array|max:2',
+            'attachments.*' => 'file|max:200|mimes:jpeg,jpg,png,pdf'
+        ]);
+        
+        if (isset($validData['date'])) {
+            $year = $incoming_letter->date->format('Y');
+            $update_date = new Carbon($validData['date']);
+            $update_year = $update_date->format('Y');
+            if ($year != $update_year) {
+                $seq_id = "CS/D/{$update_year}";
+                $cache_key = "letter_seq_{$seq_id}";
+                $number_seq = str_pad(Cache::increment($cache_key), 4, "0", STR_PAD_LEFT);
+                $incoming_letter->serial_no = "$seq_id/$number_seq";
+            }
+        }
+
+        $incoming_letter->update($validData);
+
+        if ($request->has('handovers')) {
+            $incoming_letter->handovers()->sync($validData['handovers']);
+        }
+
+        if ($request->hasFile('attachments')) {
+            $incoming_letter->attachments()->createMany(
+                array_map(function ($attachedFile) {
+                    return [
+                        'original_name' => $attachedFile->getClientOriginalName(),
+                        'path' => $attachedFile->store('/letter_attachments/incoming')
+                    ];
+                }, $request->file('attachments'))
+            );
+        }
 
         return redirect('/incoming-letters');
     }
