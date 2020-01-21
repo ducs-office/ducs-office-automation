194c687f30883631c21863cfff62e58216b83849
tanyasinghal <tanya15ds@gmail.com>
2020-01-21 13:41:48 +0530

    UI: update

--- resources/views/programmes/index.blade.php
+++ resources/views/programmes/index.blade.php
@@ -47,33 +47,39 @@
                     {{ $programme->type === 'Under Graduate(U.G.)' ? 'Under Graduate' : 'Post Graduate' }}
             </h3>
             <p class="mb-1"><span class="italic font-bold">Duration:</span> {{ $programme->duration }} year(s)</p>
-            <div class="flex">
-                <p class="mb-1"><span class="italic font-bold">Year (w.e.f) :</span> {{ $programme->wef->format('Y')}}</p>
+            <div class="flex items-center">
+                <div class="relative z-10 -ml-8 my-4">
+                    <h5 class="relative z-20 pl-8 pr-4 py-2 inline-block font-bold bg-magenta-700 text-white shadow">
+                        Latest revision (w.e.f {{ $programme->latestRevision->revised_at->format('Y') }}) 
+                    </h5>
+                    <svg class="absolute left-0 w-2 text-magenta-900" viewBox="0 0 10 10">
+                        <path fill="currentColor" d="M0 0 L10 0 L10 10 L0 0"></path>
+                    </svg>
+                </div>
                 <div class="ml-auto">
-                    <a href="{{ route('programme_revision.create', $programme) }}" class="btn btn-magenta is-sm shadow-inset mr-2"> Revise </a>
-                    <a href="{{ route('programme_revision.show', $programme) }}" class="btn btn-magenta is-sm shadow-inset mr-2">Show Versions</a>
+                    <a href="{{ route('programme_revision.create', $programme) }}" class="btn btn-magenta is-sm shadow-inset mr-2"> New Revision </a>
                 </div>            
             </div>
-            <div class="mt-3">
-                <details class="bg-gray-100 rounded-t border overflow-hidden">
-                    <summary class="p-2 bg-gray-200 cursor-pointer outline-none">
-                        Courses
-                    </summary>
-                    <div class="flex flex-wrap mx-2">
-                        @foreach($grouped_courses[$index] as $semester => $courses)
-                            <div class="p-2 w-1/2">
-                                <ul class="font-bold mb-2 p-2"> Semester - {{$semester}}
-                                    @foreach ($courses as $course)
-                                        <li class="mx-5 font-normal list-disc">{{$course->name}}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endforeach
-                    </div>
-                </details>
+            <details class="bg-gray-100 rounded-t border overflow-hidden mt-3">
+                <summary class="p-2 bg-gray-200 cursor-pointer outline-none">
+                    Courses
+                </summary>
+                <div class="flex flex-wrap mx-2">
+                    @foreach($grouped_courses[$index] as $semester => $courses)
+                        <div class="p-2 w-1/2">
+                            <ul class="font-bold mb-2 p-2"> Semester - {{$semester}}
+                                @foreach ($courses as $course)
+                                    <li class="mx-5 font-normal list-disc">{{$course->name}}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endforeach
+                </div>
+            </details>
+            <div class="mt-2">
+                <a href="{{ route('programme_revision.show', $programme) }}" class=" text-magenta-600 underline">Show all revisions</a>
             </div>
         </div>
     @endforeach
-
 </div>
 @endsection
