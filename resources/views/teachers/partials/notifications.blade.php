73fb389f6c8feafbc226e29e2cb5f87099044c43
Ruman Saleem <ruman63@gmail.com>
2020-02-21 15:37:15 +0530

    ui: notifications

--- resources/views/teachers/partials/notifications.blade.php
+++ resources/views/teachers/partials/notifications.blade.php
@@ -1,26 +1,28 @@
-<toggle-visibility class="ml-2">
+<toggle-visibility class="ml-2 relative">
     <template v-slot="{ isVisible, toggle }">
         <button class="p-1 text-blue-500" @click="toggle">
             @php
                 $notificationCount = Auth::user()->unreadNotifications->count();
             @endphp
-            <p class="h-current -mr-3 -mb-1 font-bold text-red-500" v-if="{{ $notificationCount }}"> 
+            <p class="h-current -mr-3 -mb-1 font-bold text-red-500" v-if="{{ $notificationCount }}">
                 {{ $notificationCount }}
             </p>
             <feather-icon name="bell" class="h-6">Notifications</feather-icon>
         </button>
         <transition enter-active-class="transition" leave-active-class="transition"
             enter-class="translate-x-200 opacity-0" leave-to-class="translate-x-200 opacity-0">
-            <div v-if="isVisible" class="absolute max-h-screen-1/2 bg-gray-300 z-10 p-4 border-b rounded shadow-lg">
-                <h5 class="border-b p-1 font-bold mb-2 shadow-sm">Notifications </h5>
-                @foreach (Auth::user()->unReadNotifications as $unReadNotification)
-                    <p class="mb-2 p-1 border bg-blue-200 text-gray-700 font-semibold">
-                        {{ $unReadNotification->data['inform'] }}
+            <div v-if="isVisible" class="absolute right-0 mt-2 bg-white w-72 max-h-screen-1/2 overflow-y-auto z-10 border rounded shadow-lg">
+                <h5 class="px-4 font-bold mt-4 mb-2 shadow-sm">Notifications</h5>
+                @forelse (Auth::user()->notifications as $notification)
+                    <p class="px-4 py-2 border-t{{ $notification->read_at ? ' bg-gray-100 ' : ' bg-white font-bold ' }}hover:bg-gray-200">
+                        {{ $notification->data['inform'] }}
                     </p>
                     @php
-                        $unReadNotification->markAsRead();
+                        $notification->markAsRead();
                     @endphp
-                @endforeach
+                @empty
+                    <p class="text-gray-500 font-bold text-center">No notifications.</p>
+                @endforelse
             </div>
         </transition>
     </template>
