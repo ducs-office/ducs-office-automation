c574fbe6eea56cd41f1148a0baf4e7b2cce9a172
Swati Gautam <swati.mcs18.du@gmail.com>
2020-03-06 16:20:29 +0530

    Scholar profile UI

--- resources/views/scholars/edit.blade.php
+++ resources/views/scholars/edit.blade.php
@@ -2,8 +2,8 @@
 @section('body')
     <div class="container mx-auto p-4">
         <form class="bg-white p-6 h-full shadow-md" action="{{ route('scholars.profile.update') }}"
-        method="POST" enctype="multipart/form-data">
-        @csrf_token @method('PATCH')
+            method="POST" enctype="multipart/form-data">
+            @csrf_token @method('PATCH')
             <div class="flex items-center mb-4">
                 <image-upload-input id="profile_picture"
                     name="profile_picture"
@@ -18,40 +18,65 @@
                     </template>
                 </image-upload-input>
             </div>
-            <div class="mb-2">
-                <label for="address" class="block form-label mb-1">Address</label>
-                <textarea id="address" name="address" class="block w-auto form-input">{{ old('address', $scholar->address) }}</textarea>
+            <div class="relative z-10 -ml-6 my-4 mt-8">
+                <h5 class="relative z-20 pl-4 pr-4 py-2 inline-block font-bold bg-magenta-700 text-white shadow">
+                    Personal Details
+                </h5>
             </div>
-            <div class="mb-2">
-                <label for="email" class="block form-label mb-1">Email</label>
+            <div class="flex items-baseline ">
+                <label for="gender" class="block form-label mr-2"> Gender:</label>
+                <select id="gender" name="gender" class="block form-input">
+                    <option value="" class="text-gray-600" selected>Select your gender </option>
+                    @foreach ($genders as $acronym => $gender)
+                    <option value=" {{ $acronym }}"
+                        {{ $acronym === old("gender", $scholar->gender) ? 'selected': '' }}>
+                        {{ $gender }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="flex items-baseline mt-2">
+                <label for="category" class="block form-label mr-2"> Category:</label>
+                <select id="category" name="category" class="block form-input">
+                    <option value="" class="text-gray-600" selected>Choose a category </option>
+                    @foreach ($categories as $acronym => $category)
+                    <option value=" {{ $acronym }}"
+                        {{ $acronym === old("category", $scholar->category) ? 'selected': '' }}>
+                        {{ $category }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mt-2 flex items-baseline ">
+                <label for="email" class="block form-label">Email:</label>
                 <input id="email" type="email" name="email"
-                    class="block w-auto form-input"
+                    class="block w-auto form-input ml-2"
                     disabled
                     value="{{ $scholar->email }}">
             </div>
-            <div class="mb-2">
-                <label for="phone_no" class="block form-label mb-1">Phone Number</label>
+            <div class="mt-2 flex items-baseline ">
+                <label for="phone_no" class="block form-label">Phone Number:</label>
                 <input id="phone_no" type="text" name="phone_no" class="block w-auto form-input" value="{{ old('phone_no', $scholar->phone_no) }}">
             </div>
-            <div class="mt-4 mb-2">
-                <h3 class="font-bold mb-2"> Admission Details</h3>
-                <div class="flex items-baseline ">
-                    <label for="category" class="block form-label mr-2"> Category:</label>
-                    <select id="category" name="category" class="block form-input">
-                        <option value="" selected>Choose a category </option>
-                        @foreach ($categories as $acronym => $category)
-                        <option value=" {{ $acronym }}"
-                            {{ $acronym === old("category", $scholar->category) ? 'selected': '' }}>
-                            {{ $category }}
-                        </option>
-                        @endforeach
-                    </select>
+            <div class="mt-2 flex items-baseline ">
+                <label for="address" class="block form-label mb-1">Address:</label>
+                <textarea id="address" name="address" class="block w-auto form-input ml-2">{{ old('address', $scholar->address) }}</textarea>
+            </div>
+            <div class="relative z-10 -ml-6 my-4 mt-8">
+                <h5 class="relative z-20 pl-4 pr-4 py-2 inline-block font-bold bg-magenta-700 text-white shadow">
+                    Admission Details
+                </h5>
+            </div>
+            <div class="mt-4">
+                <div class="mt-2 flex items-baseline ">
+                    <label for="enrollment_date" class="block form-label">Date of enrollment:</label>
+                    <input id="enrollment_date" type="date" name="enrollment_date" class="block w-auto form-input ml-2" value="{{ old('phone_no', $scholar->enrollment_date) }}">
                 </div>
                 <div class="flex items-baseline mt-2">
                     <label for="admission_via" class="block form-label mr-2">Admission Via:</label>
                     <select id="admission_via" name="admission_via" class="block form-input">
                         <option value="" selected> Choose the mode of admission </option>
-                        @foreach ($admission_criterias as $acronym => $admission_via)
+                        @foreach ($admissionCriterias as $acronym => $admission_via)
                         <option value=" {{ $acronym }}"
                             {{ $acronym === old("admission_via", $scholar->admission_via) ? 'selected': '' }}>
                             {{ $admission_via['mode'] }}
@@ -59,6 +84,29 @@
                         @endforeach
                     </select>
                 </div>
+            </div>
+            <div class="relative z-10 -ml-6 my-4 mt-8">
+                <h5 class="relative z-20 pl-4 pr-4 py-2 inline-block font-bold bg-magenta-700 text-white shadow">
+                    Research Details
+                </h5>
+            </div>
+            <div class="mt-4">
+                <div class="mt-2 flex items-baseline ">
+                    <label for="research_area" class="block form-label"> Broad Area of Research: </label>
+                    <textarea id="research_area"  name="research_area" class="block w-auto form-input ml-2">{{ old('research_area', $scholar->research_area) }}</textarea>
+                </div>
+            </div>
+            <div class="flex items-baseline mt-2">
+                <label for="supervisor_profile_id" class="block form-label mr-2"> Supervisor </label>
+                <select name="supervisor_profile_id" id="supervisor" class="block form-input">
+                    <option value="" class="text-gray-600" selected> Select your supervisor </option>
+                    @foreach ($supervisorProfiles as $name => $id)
+                        <option value=" {{ $id }} "
+                            {{ $id === old('supervisor_profile_id', $scholar->supervisor_profile_id)? 'selected':''}} >
+                            {{ $name }}
+                        </option>
+                    @endforeach
+                </select>
             </div>
             <div class="mt-5">
                 <button type="submit" class="btn btn-magenta">Save Changes</button>
--- resources/views/scholars/profile.blade.php
+++ resources/views/scholars/profile.blade.php
@@ -9,69 +9,98 @@
                     <a href=" {{ route('scholars.profile.edit') }} " class="btn btn-magenta">Edit</a>
                 </div>
             </div>
-            <div>
-                <p> {{ $scholar->enrollment_date }}</p>
+            <div class="relative z-10 -ml-6 my-4 mt-8">
+                <h5 class="relative z-20 pl-4 pr-4 py-2 inline-block font-bold bg-magenta-700 text-white shadow">
+                    Personal Details
+                </h5>
             </div>
-            <div>
-                <p> {{ $genders[$scholar->gender] }}</p>
+            <div class="mt-2 flex">
+                <h4 class="font-semibold"> Gender: </h4>
+                <p class="ml-2"> {{ $genders[$scholar->gender] }}</p>
             </div>
-            <div>
-                <p> {{ $scholar->research_area}} </p>
+            <div class="flex mt-2">
+                <p class="font-semibold"> Category:</p>
+                <p class="ml-2"> {{$categories[$scholar->category] ?? 'not set'}}</p>
             </div>
-            <address>
-                {{ $scholar->address}}
-            </address>
-            <p class="flex items-center">
-                <feather-icon name="at-sign" class="h-current mr-2"></feather-icon>
-                <a href="mailto:{{ $scholar->email}}">{{ $scholar->email }}</a>
-            </p>
-            <p class="flex items-center">
-                <feather-icon name="phone" class="h-current mr-2"></feather-icon>
-                <a href="tel:{{ $scholar->phone_no }}">{{ $scholar->phone_no }}</a>
-            </p>
-            <div class="mt-4 mb-2">
-                <h3 class="font-bold mb-4"> Admission Details</h3>
+            <div class="mt-2">
+                <p class="flex items-center mb-1">
+                    <feather-icon name="at-sign" class="h-current mr-2"></feather-icon>
+                    <a href="mailto:{{ $scholar->email}}">{{ $scholar->email }}</a>
+                </p>
+                <p class="flex items-center mb-1">
+                    <feather-icon name="phone" class="h-current mr-2"></feather-icon>
+                    <a href="tel:{{ $scholar->phone_no }}">{{ $scholar->phone_no }}</a>
+                </p>
                 <div class="flex">
-                    <p class="font-semibold"> Category:</p>
-                    <p class="ml-2"> {{$categories[$scholar->category] ?? 'not set'}}</p>
-                </div>
-                <div class="flex">
-                    <div class="flex">
-                        <p class="font-semibold"> Admission via: </p>
-                        <p class="ml-2"> {{ $admission_criterias[$scholar->admission_via]['mode'] ?? 'not set'}}</p>
-                    </div>
-                    
-                    <div class="ml-4 flex">
-                        <p class="font-semibold"> Funding: </p>
-                        <p class="ml-2"> {{ $admission_criterias[$scholar->admission_via]['funding'] ?? 'not set'}}</p>
-                    </div>
+                    <feather-icon name="home" class="h-current mr-2"></feather-icon>
+                    <address>
+                        {{ $scholar->address}}
+                    </address>
                 </div>
             </div>
+            <div class="relative z-10 -ml-6 my-4 mt-8">
+                <h5 class="relative z-20 pl-4 pr-4 py-2 inline-block font-bold bg-magenta-700 text-white shadow">
+                    Admission Details
+                </h5>
+            </div>
+            <div class="mt-2 flex">
+                <h4 class="font-semibold"> Date of enrollment: </h4>
+                <p class="ml-2"> {{ $scholar->enrollment_date }}</p>
+            </div>
+            <div class="flex mt-2">
+                <p class="font-semibold"> Admission via: </p>
+                <p class="ml-2"> {{ $admissionCriterias[$scholar->admission_via]['mode'] ?? 'not set'}}</p>
+            </div>
+            
+            <div class="mt-2 flex">
+                <p class="font-semibold"> Funding: </p>
+                <p class="ml-2"> {{ $admissionCriterias[$scholar->admission_via]['funding'] ?? 'not set'}}</p>
+            </div>
+            <div class="relative z-10 -ml-6 my-4 mt-8">
+                <h5 class="relative z-20 pl-4 pr-4 py-2 inline-block font-bold bg-magenta-700 text-white shadow">
+                    Research Details
+                </h5>
+            </div>
+            <div class="mt-2 flex">
+                <h4 class="font-semibold"> Broad Area of Research: </h4>
+                <p class="ml-2"> {{ $scholar->research_area }}</p>
+            </div> 
             <div class="mt-4">
                 <h3 class="font-bold"> Supervisor </h3>
-                <p> {{ $scholar->supervisor->name }} </p>
+            </div>
+            <div class="flex items-center mt-2">
+                <svg viewBox="0 0 20 20" class="h-current">
+                    <g stroke="none" stroke-width="1" fill="currentColor" fill-rule="evenodd">
+                        <path d="M3.33333333,8 L10,12 L20,6 L10,0 L-5.55111512e-16,6 L10,6 L10,6.5 L10,8 L3.33333333,8 L3.33333333,8 Z M1.33226763e-15,8 L1.33226763e-15,16 L2,13.7777778 L2,9.2 L2,9.2 L1.33226763e-15,8 L1.11022302e-16,8 L1.33226763e-15,8 Z M10,20 L5,16.9999998 L3,15.8 L3,9.8 L10,14 L17,9.8 L17,15.8 L10,20 L10,20 Z"></path>
+                    </g>
+                </svg>
+                <p class="ml-2 text"> {{ $scholar->supervisor->name }}  </p>
             </div>
             <div class="mt-4">
                 <h3 class="font-bold">Co-Supervisors</h3>
-                <div class="mt-2">
-                    @foreach ($scholar->co_supervisors as $co_supervisor)
-                        <p> {{ $co_supervisor['title'] }} </p>
-                        <p> {{ $co_supervisor['name'] }} </p>
-                        <p> {{ $co_supervisor['designation'] }} </p>
-                        <p> {{ $co_supervisor['affiliation'] }} </p>
-                    @endforeach
-                </div>
+                @foreach ($scholar->co_supervisors as $coSupervisor)
+                    <div class="mt-2">
+                        <div class="flex">
+                            <feather-icon name="pen-tool" class="h-current"></feather-icon>
+                            <p class="ml-2"> {{ $coSupervisor['title'] }} {{ $coSupervisor['name'] }} </p>
+                        </div>
+                        <p class="ml-6 text-gray-700"> {{ $coSupervisor['designation'] }} </p>
+                        <p class="ml-6 text-gray-700"> {{ $coSupervisor['affiliation'] }} </p>
+                    </div>
+                @endforeach
             </div>
             <div class="mt-4">
                 <h3 class="font-bold">Advisory Committee</h3>
-                <div mt-2>
-                    @foreach ($scholar->advisory_committee as $advisory)
-                        <p> {{ $advisory['title'] }} </p>
-                        <p> {{ $advisory['name'] }} </p>
-                        <p> {{ $advisory['designation'] }} </p>
-                        <p> {{ $advisory['affiliation'] }} </p>
-                    @endforeach
-                </div>
+                @foreach ($scholar->advisory_committee as $adviser)
+                    <div class="mt-2">
+                        <div class="flex">
+                            <feather-icon name="pen-tool" class="h-current"></feather-icon>
+                            <p class="ml-2"> {{ $adviser['title'] }} {{ $adviser['name'] }} </p>
+                        </div>
+                        <p class="ml-6 text-gray-700"> {{ $adviser['designation'] }} </p>
+                        <p class="ml-6 text-gray-700"> {{ $adviser['affiliation'] }} </p>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
