62ede214822382158c2a34912d53efbe4a76859c
Ruman Saleem <ruman63@gmail.com>
2020-06-09 23:04:57 +0530

    consistent sidebar link styles

--- resources/views/_partials/layouts/scholar-sidebar.blade.php
+++ resources/views/_partials/layouts/scholar-sidebar.blade.php
@@ -7,61 +7,61 @@
 <aside class="flex-1 px-4">
     <ul class="font-bold text-white-90 space-y-2">
         <li>
-            <a href="{{ route('scholars.dashboard') }}" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
+            <a href="{{ route('scholars.dashboard') }}" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:scale-105 focus:scale-105 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
                 <x-feather-icon name="grid" class="h-5 mr-4" stroke-width="2">Dashboard</x-feather-icon>
                 Dashboard
             </a>
         </li>
         <li>
-            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
+            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:scale-105 focus:scale-105 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
                 <x-feather-icon name="book" class="h-5 mr-4" stroke-width="2">Coursework</x-feather-icon>
                 Pre-PhD Courseworks
             </a>
         </li>
         <li>
-            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
+            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:scale-105 focus:scale-105 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
                 <x-feather-icon name="file" class="h-5 mr-4" stroke-width="2">Publications</x-feather-icon>
                 Publications
             </a>
         </li>
         <li>
-            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
+            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:scale-105 focus:scale-105 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
                 <x-feather-icon name="monitor" class="h-5 mr-4" stroke-width="2">Presentation</x-feather-icon>
                 Presentations
             </a>
         </li>
         <li>
-            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
+            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:scale-105 focus:scale-105 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
                 <x-feather-icon name="umbrella" class="h-5 mr-4" stroke-width="2">Leaves</x-feather-icon>
                 Leaves
             </a>
         </li>
         <li>
-            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
+            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:scale-105 focus:scale-105 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
                 <x-feather-icon name="briefcase" class="h-5 mr-4" stroke-width="2"></x-feather-icon>
                 Advisory Meetings
             </a>
         </li>
         <li>
-            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
+            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:scale-105 focus:scale-105 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
                 <x-feather-icon name="trending-up" class="h-5 mr-4" stroke-width="2"></x-feather-icon>
                 Progress Reports
             </a>
         </li>
         <li>
-            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
+            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:scale-105 focus:scale-105 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
                 <x-feather-icon name="paperclip" class="h-5 mr-4" stroke-width="2"></x-feather-icon>
                 Documents
             </a>
         </li>
         <li>
-            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
+            <a href="#" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:scale-105 focus:scale-105 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
                 <x-feather-icon name="flag" class="h-5 mr-4" stroke-width="2"></x-feather-icon>
                 Examiner Status
             </a>
         </li>
         <li>
-            <a href="{{ route('scholars.profile.show', auth()->user()) }}" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
+            <a href="{{ route('scholars.profile.show', auth()->user()) }}" class="flex items-center py-2 px-4 text-white-70 rounded transform transition-transform duration-150 hover:scale-105 focus:scale-105 hover:-translate-y-1 focus:-translate-y-1 hover:text-white focus:text-white hover:bg-magenta-700 focus:bg-magenta-700 hover:shadow focus:shadow focus:outline-none">
                 <x-feather-icon name="user" class="h-5 mr-4" stroke-width="2"></x-feather-icon>
                 Profile
             </a>
