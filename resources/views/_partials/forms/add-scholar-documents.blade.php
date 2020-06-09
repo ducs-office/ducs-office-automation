cb0c3c7970f3e2ec7d026da8d5e814f70a316ccd
tanyasinghal <tanya15ds@gmail.com>
2020-06-10 00:58:43 +0530

    fix scholar documents

---
+++ resources/views/_partials/forms/add-scholar-documents.blade.php
@@ -0,0 +1,56 @@
+<form action="{{ route('scholars.documents.store', $scholar) }}" method="POST"
+    class="px-6" enctype="multipart/form-data"
+    x-data="{type: '{{ old('type') }}'}">
+    @csrf_token
+    <div class="mb-2 items-center">
+        <div class="mb-2">
+            <label for="date" class="mb-1 w-full form-label @error('date') text-red-500 @enderror">Date
+                <span class="text-red-600">*</span>
+            </label>
+            <input type="date" name="date" id="date" class="w-full form-input @error('date') border-red-500 hover:border-red-700 @enderror" 
+                value="{{ old('date') }}" 
+                required>
+        @error('date')
+            <p class="text-red-500"> {{ $message }} </p>
+        @enderror
+        </div>
+        <div class="mb-2">
+            <label for="document" class="mb-1 w-full form-label @error('document') text-red-500 @enderror">Upload Document
+                <span class="text-red-600">*</span>
+            </label>
+            <x-input.file name="document" id="document"
+            class="w-full form-input inline-flex items-center {{ $errors->has('document') ? 'border-red-500 hover:border-red-700' : '' }}"
+            accept="application/pdf, image/*"
+            placeholder="Upload Document"
+            required/>
+        @error('document')
+            <p class="text-red-500"> {{ $message }} </p>
+        @enderror
+        </div>
+        <div class="mb-2">
+            <label for="type" class="mb-1 form-label w-full @error('type') text-red-500 @enderror">
+                Type <span class="text-red-600">*</span>
+            </label>
+            <select name="type" id="type" 
+            class="form-select w-full @error('type') border-red-500 hover:border-red-700 @enderror"
+            x-model="type">
+                @foreach ($documentTypes as $documentType)
+                    <option value="{{ $documentType }}">
+                        {{$documentType}} </option>
+                @endforeach
+            </select>
+        @error('type')
+            <p class="text-red-500"> {{ $message }} </p>
+        @enderror
+        </div>
+        <div class="mb-2">
+            <label for="description" class="mb-1 w-full form-label @error('description') text-red-500 @enderror">Description
+            </label>
+            <textarea id="description" name="description" type="" class="w-full form-input @error('description') border-red-500 hover:border-red-700 @enderror" placeholder="Enter Description">{{ old('description') }}</textarea>
+        @error('description')
+            <p class="text-red-500"> {{ $message }} </p>
+        @enderror
+        </div>
+    </div>
+    <button type="submit" class="px-5 btn btn-magenta text-sm rounded-l-none">Add</button>
+</form>
--- resources/views/_partials/research/scholar-profile/documents.blade.php
+++ resources/views/_partials/research/scholar-profile/documents.blade.php
@@ -1,4 +1,10 @@
 {{--Other Documents--}}
+@push('modals')
+<x-modal name="add-documents-modal" class="p-6 min-w-1/2" :open="! $errors->default->isEmpty()">
+    <h3 class="text-lg font-bold mb-4">Add Documents</h3>
+    @include('_partials.forms.add-scholar-documents')
+</x-modal>
+@endpush
 <div class="page-card p-6 flex overflow-visible space-x-6">
     <div class="w-64 pr-4 relative z-10 -ml-8 my-2">
         <h3 class="relative pl-8 pr-4 py-2 font-bold bg-magenta-700 text-white shadow">
@@ -25,13 +31,13 @@
                         </a>
                         @can('delete', $document)
                         <form method="POST" action="{{ route('scholars.documents.destroy', [$scholar, $document]) }}"
-                                onsubmit="return confirm('Do you really want to delete this document?');">
-                                @csrf_token
-                                @method('DELETE')
-                                <button type="submit" class="p-1 hover:bg-gray-200 text-red-700 rounded">
-                                    <x-feather-icon name="trash-2" stroke-width="2.5" class="h-current">Delete</x-feather-icon>
-                                </button>
-                            </form>
+                            onsubmit="return confirm('Do you really want to delete this document?');">
+                            @csrf_token
+                            @method('DELETE')
+                            <button type="submit" class="p-1 hover:bg-gray-200 text-red-700 rounded">
+                                <x-feather-icon name="trash-2" stroke-width="2.5" class="h-current">Delete</x-feather-icon>
+                            </button>
+                        </form>
                         @endcan
                     </div>
                 </li>
@@ -41,50 +47,9 @@
             @endforelse
         </ul>
         @can('create', App\Models\ScholarDocument::class)
-        <button class="mt-2 w-full btn btn-magenta rounded-lg py-3" @click="$modal.show('add-documents-modal')">
+        <x-modal.trigger class="mt-2 w-full btn btn-magenta rounded-lg py-3" modal="add-documents-modal">
             + Add Documents
-        </button>
+        </x-modal.trigger>
         @endcan
-        <x-modal name="add-documents-modal" height="auto">
-            <div class="p-6">
-                <h3 class="text-lg font-bold mb-4">Add Documents</h3>
-                <form action="{{ route('scholars.documents.store', $scholar) }}" method="POST"
-                    class="px-6" enctype="multipart/form-data">
-                    @csrf_token
-                    <div class="mb-2 items-center">
-                        <div class="mb-2">
-                            <label for="date" class="mb-1 w-full form-label">Date
-                                <span class="text-red-600">*</span>
-                            </label>
-                            <input type="date" name="date" id="date" class="w-full form-input" required>
-                        </div>
-                        <div class="mb-2">
-                            <label for="document" class="mb-1 w-full form-label">Upload Document
-                                <span class="text-red-600">*</span>
-                            </label>
-                            <input type="file" name="document" id="">
-                        </div>
-                        <div class="mb-2">
-                            <label for="type" class="mb-1 form-label w-full">
-                                Type <span class="text-red-600">*</span>
-                            </label>
-                            <select name="type" id="type" class="form-select w-full">
-                                @foreach ($documentTypes as $documentType)
-                                    <option value="{{ $documentType }}">
-                                        {{$documentType}} </option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="mb-2">
-                            <label for="description" class="mb-1 w-full form-label">Description
-                            </label>
-                            <textarea id="description" name="description" type="" class="w-full form-input" placeholder="Enter Description" required>
-                            </textarea>
-                        </div>
-                    </div>
-                    <button type="submit" class="px-5 btn btn-magenta text-sm rounded-l-none">Add</button>
-                </form>
-            </div>
-        </x-modal>
     </div>
 </div>
