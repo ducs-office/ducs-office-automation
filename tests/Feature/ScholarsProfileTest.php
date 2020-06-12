be59f4bf85e53f0abea919771e6374f12697da71
Swati Gautam <swati.mcs18.du@gmail.com>
2020-06-12 12:35:08 +0530

    fix test

--- tests/Feature/ScholarsProfileTest.php
+++ tests/Feature/ScholarsProfileTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Scholar;
 use App\Models\ScholarProfile;
 use App\Models\Teacher;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -17,6 +18,9 @@ class ScholarsProfileTest extends TestCase
     public function scholar_can_view_their_profile()
     {
         $this->signInScholar($scholar = create(Scholar::class));
+
+        $supervisor = factory(User::class)->states('supervisor')->create();
+        $scholar->supervisors()->attach($supervisor);
 
         $response = $this->withoutExceptionHandling()
             ->get(route('scholars.profile.show', $scholar))
